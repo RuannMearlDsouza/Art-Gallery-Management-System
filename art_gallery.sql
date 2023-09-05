db432a8ef081c49d94da6eda426ca4c6b0b2f460
Priyanka S Mayya <priyankasmayya@gmail.com>
2023-09-05 20:56:15 +0530

    Add files via upload

---
+++ art_gallery.sql
@@ -0,0 +1,219 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 05, 2023 at 05:25 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `art_gallery`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `create_backup` (IN `id` INT, IN `c_name` VARCHAR(20) CHARSET utf8mb4, IN `c_phone` VARCHAR(10) CHARSET utf8mb4, IN `c_gender` VARCHAR(6) CHARSET utf8mb4, IN `c_email` VARCHAR(50) CHARSET utf8mb4, IN `c_password` VARCHAR(20) CHARSET utf8mb4)  MODIFIES SQL DATA BEGIN
+INSERT INTO backup(cus_id,name,phone,gender,email,password)VALUES(id,c_name,c_phone,c_gender,c_email,c_password);
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`email`, `password`) VALUES
+('admin@artgallery.com', '12345678');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artist`
+--
+
+CREATE TABLE `artist` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `approved` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `artist`
+--
+
+INSERT INTO `artist` (`id`, `name`, `phone`, `gender`, `email`, `password`, `approved`) VALUES
+(1, 'Priyanka Art', '9876543210', 'female', 'priyankaart@gmail.com', '12345678', 'true'),
+(2, 'Sameeksha', '1234567890', 'female', 'sameeksha@art.com', '1234567890', 'true');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `backup`
+--
+
+CREATE TABLE `backup` (
+  `cus_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `backup`
+--
+
+INSERT INTO `backup` (`cus_id`, `name`, `phone`, `gender`, `email`, `password`) VALUES
+(1, 'Priyanka S Mayya', '8762114892', 'female', 'priyankasmayya@gmail.com', '12345678'),
+(2, 'Priyanka S Mayya', '9876543210', 'female', '21j35.priyanka@sjec.ac.in', '1234567890');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `cus_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`cus_id`, `name`, `phone`, `gender`, `email`, `password`) VALUES
+(1, 'Priyanka S Mayya', '8762114892', 'female', 'priyankasmayya@gmail.com', '12345678'),
+(2, 'Priyanka S Mayya', '9876543210', 'female', '21j35.priyanka@sjec.ac.in', '1234567890');
+
+--
+-- Triggers `customer`
+--
+DELIMITER $$
+CREATE TRIGGER `on_new_customer` AFTER INSERT ON `customer` FOR EACH ROW BEGIN
+call create_backup(new.cus_id,new.name,new.phone,new.gender,new.email,new.password);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `idGallery` int(11) NOT NULL,
+  `titleGallery` longtext NOT NULL,
+  `descGallery` longtext NOT NULL,
+  `imgFullNameGallery` longtext NOT NULL,
+  `orderGallery` int(11) NOT NULL,
+  `art_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `gallery`
+--
+
+INSERT INTO `gallery` (`idGallery`, `titleGallery`, `descGallery`, `imgFullNameGallery`, `orderGallery`, `art_id`) VALUES
+(4, 'painting', 'Famous Painting', 'painting.64f4bcc7a4a110.23609218.jpg', 1, 1),
+(5, 'Art', 'Pastel colours', 'painting-2.64f5b3e1c83fa5.86444463.jpg', 2, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `artist`
+--
+ALTER TABLE `artist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `backup`
+--
+ALTER TABLE `backup`
+  ADD PRIMARY KEY (`cus_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`cus_id`);
+
+--
+-- Indexes for table `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`idGallery`),
+  ADD KEY `art_id` (`art_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `artist`
+--
+ALTER TABLE `artist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `cus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `idGallery` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `gallery`
+--
+ALTER TABLE `gallery`
+  ADD CONSTRAINT `gallery_ibfk_1` FOREIGN KEY (`art_id`) REFERENCES `artist` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
